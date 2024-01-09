c8ed453f13418e1884cc661fdf67a6e72d84955e
HanayoSugiyama <148828272+HanayoSugiyama@users.noreply.github.com>
2024-01-09 22:32:37 +0900

    ..

--- js/script.js
+++ js/script.js
@@ -4,48 +4,6 @@ setTimeout(function() {
   document.querySelector('.top__gif').style.display = 'none';
 }, 7500);
 
-/* glowAnimeにglowというクラス名を付ける定義
-function GlowAnimeControl() {
-  $('.glowAnime').each(function () {
-    var elemPos = $(this).offset().top - 50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight) {
-      $(this).addClass("glow");
-
-    } else {
-      $(this).removeClass("glow");
-    }
-  });
-}
-
-// 画面が読み込まれたら5秒後に動かしたい場合の記述
-$(document).ready(function () {
-  setTimeout(function(){
-    //spanタグを追加する
-    var element = $(".glowAnime");
-    element.each(function () {
-      var text = $(this).text();
-      var textbox = "";
-      text.split('').forEach(function (t, i) {
-        if (t !== " ") {
-          if (i < 10) {
-            textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
-          } else {
-            var n = i / 10;
-            textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
-          }
-
-        } else {
-          textbox += t;
-        }
-      });
-      $(this).html(textbox);
-    });
-  }, 500);
-  GlowAnimeControl(); // アニメーション用の関数を呼ぶ
-});  //ここまで画面が読み込まれたらすぐに動かしたい場合の記述*/
-
 
 //モーダル
 $(".video-open").modaal({
@@ -99,6 +57,9 @@ const getScrollY = () => {
 
 window.addEventListener('scroll', getScrollY);
 //ここまでTOPへ戻るの設定
+
+
+
 
 var navPos = jQuery( '#global-nav' ).offset().top; // グローバルメニューの位置
 var navHeight = jQuery( '#global-nav' ).outerHeight(); // グローバルメニューの高さ
