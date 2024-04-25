83d2193c29373f52e9a71a0475c1da05c50943b4
HanayoSugiyama <bluerose.87.hana@gmail.com>
2024-04-25 16:07:23 +0900

    **

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 'use strict';
 
-setTimeout(function() {
+setTimeout(function () {
   document.querySelector('.top__gif').style.display = 'none';
 }, 6500);
 
 
 //モーダル
 $(".video-open").modaal({
-	type: 'video',
-	overlay_close:true,//モーダル背景クリック時に閉じるか
-	background: '#28BFE7', // 背景色
-	overlay_opacity:0.8, // 透過具合
-    youtube: {
-       autoplay: 1,
-       mute: 1,
-   }
+  type: 'video',
+  overlay_close: true, //モーダル背景クリック時に閉じるか
+  background: '#28BFE7', // 背景色
+  overlay_opacity: 0.8, // 透過具合
+  youtube: {
+    autoplay: 1,
+    mute: 1,
+  }
 });
 //モーダルここまで
 
 //スライダー
 if (window.matchMedia("(max-width: 768px)").matches) {
-  document.getElementsByClassName("slider")[0].classList.remove("slider");}
+  document.getElementsByClassName("slider")[0].classList.remove("slider");
+}
 
 $('.slider').slick({
-  autoplay: true,//自動的に動き出す。
-  infinite: true,//スライドをループさせる。
-  speed: 500,//スライドのスピード。
-  slidesToShow: 3,//スライドを画面に3枚見せる
-  slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-  centerMode: true,//要素を中央ぞろえにする
-  variableWidth: true,//幅の違う画像の高さを揃えて表示
-  dots: true,//下部ドットナビゲーションの表示
- 
+  autoplay: true, //自動的に動き出す。
+  infinite: true, //スライドをループさせる。
+  speed: 500, //スライドのスピード。
+  slidesToShow: 3, //スライドを画面に3枚見せる
+  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  centerMode: true, //要素を中央ぞろえにする
+  variableWidth: true, //幅の違う画像の高さを揃えて表示
+  dots: true, //下部ドットナビゲーションの表示
+
 });
 
 
@@ -46,9 +47,9 @@ const getScrollY = () => {
   const pageTop = document.querySelector('#page-top');
 
   if (scrolled >= 500) {
-      pageTop.classList.add('open')
+    pageTop.classList.add('open')
   } else {
-      pageTop.classList.remove('open')
+    pageTop.classList.remove('open')
   }
 };
 
@@ -58,14 +59,14 @@ window.addEventListener('scroll', getScrollY);
 
 
 
-var navPos = jQuery( '#global-nav' ).offset().top; // グローバルメニューの位置
-var navHeight = jQuery( '#global-nav' ).outerHeight(); // グローバルメニューの高さ
-jQuery( window ).on( 'scroll', function() {
-	if ( jQuery( this ).scrollTop() > navPos ) {
-		jQuery( 'body' ).css( 'padding-top', navHeight );
-		jQuery( '#global-nav' ).addClass( 'm_fixed' );
-	} else {
-		jQuery( 'body' ).css( 'padding-top', 0 );
-		jQuery( '#global-nav' ).removeClass( 'm_fixed' );
-	}
+var navPos = jQuery('#global-nav').offset().top; // グローバルメニューの位置
+var navHeight = jQuery('#global-nav').outerHeight(); // グローバルメニューの高さ
+jQuery(window).on('scroll', function () {
+  if (jQuery(this).scrollTop() > navPos) {
+    jQuery('body').css('padding-top', navHeight);
+    jQuery('#global-nav').addClass('m_fixed');
+  } else {
+    jQuery('body').css('padding-top', 0);
+    jQuery('#global-nav').removeClass('m_fixed');
+  }
 });
